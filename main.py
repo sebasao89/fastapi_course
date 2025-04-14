c4319f32da7e8498c43ed58f9e657cbc83701819
Sebasao <sebas.ao@outlook.com>
2025-04-14 17:01:47 -0500

    add requirements

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ country_timezones = {
 
 current_id: int = 0
 
+db_customers: list[Customer] = []
+
 
 # ENDPOINTS
 
@@ -28,7 +30,7 @@ def read_root():
 async def time():
     return {"Time": datetime.now()}
 
-# endpoint with a variable
+    # endpoint with a variable
 @myapp.get("/time2/{variable_iso_code}")
 async def time(variable_iso_code: str):
     iso = variable_iso_code.upper()
@@ -36,12 +38,24 @@ async def time(variable_iso_code: str):
     tz = zoneinfo.ZoneInfo(timezone_str)
     return {"Time": datetime.now(tz)}
 
-@myapp.post("/customer", response_model=Customer) #response_model es para que devuelva el modelo de Customer
-async def create_customer(customer_data: CustomerCreate): #Este modelo es el que se usa para crear un cliente
-    customer = Customer.model_validate(customer_data.model_dump) #model_validate es para validar el modelo que se ingresan, y debemos pasar un diccionario
-    customer.id = current_id + 1
+@myapp.post("/customer", response_model=Customer)   #response_model es para que devuelva el modelo de Customer
+async def create_customer(customer_data: CustomerCreate):   #Este modelo es el que se usa para crear un cliente
+    customer = Customer.model_validate(customer_data.model_dump())  #model_validate es para validar el modelo que ingresan, y debemos pasar un diccionario
+    customer.id = len(db_customers)
+    db_customers.append(customer)
     #return customer_data           #customer_data es el que se pasa como parámetro, y 
-    return customer #customer es el que se devuelve como respuesta
+    return customer     #customer es el que se devuelve como respuesta
+
+@myapp.get("/customers", response_model=list[Customer])  #response_model es para que devuelva una lista de Customer
+async def get_customers():
+    return db_customers
+
+@myapp.get("/customer/{customer_id}", response_model=Customer)  #response_model es para que devuelva un Customer
+async def get_customer(customer_id: int):
+    for customer in db_customers:
+        if customer.id == customer_id:
+            return customer
+    return {"message": "Customer not found"}
 
 @myapp.post("/transaction")
 async def create_transaction(transaction_data: Transaction):
